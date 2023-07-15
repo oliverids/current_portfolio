e9ac85022dc3e8f65d8e941a74cfd20c9568cfea
Isabela Oliveira <oliver.isabela2303@gmail.com>
2023-07-15 19:46:57 -0300

    feat: internal links from nav scroll bug fixes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 const menu_button = document.getElementById('menu'),
-  header = document.querySelector('header');
+  header = document.querySelector('header'),
+  internal_links = document.querySelectorAll('#internal_nav a');
 
 menu_button.addEventListener('click', () => {
   header.classList.toggle('ativo');
 })
 
+internal_links.forEach(each => {
+  each.addEventListener('click', e => {
+    e.preventDefault();
+
+    const href = event.currentTarget.getAttribute('href'),
+      section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    header.classList.remove('ativo')
+  })
+})
+
 let topo;
 window.addEventListener('scroll', e => {
   window.scrollY !== 0 ? header.classList.add('scroll') : header.classList.remove('scroll');
-
-  // topo = document.querySelector('#projetos').getBoundingClientRect().top;
-  // topo < -400 ? backTop.classList.add('show') : backTop.classList.remove('show');
 });
